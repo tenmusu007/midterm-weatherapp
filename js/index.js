580ff4caed899b1f0c0a275cfcbe31374d540558
Atsuya Tanaka <tenmusu007@gmail.com>
2022-06-13 11:10:16 -0700

    init

--- js/index.js
+++ js/index.js
@@ -14,5 +14,19 @@ export async function gettingData(city = "vancouver"){
         console.error(error);
     }
 }
+export async function gettingDataFor(city = "vancouver"){
+    // console.log(city)
+    try{
+        // const response = await fetch(`https:api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey.weather}&units=metric`)
+        const response = await fetch(`https:api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey.weather}&units=metric`)
+        // console.log(response)
+        const myJson = await response.json()
+        let data = {...myJson};
+
+        return data       
+    }catch(error){
+        console.error(error);
+    }
+}
 
 
