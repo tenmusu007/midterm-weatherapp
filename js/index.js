8f2bd1b213c5c796335d3a30309f025eb9726635
Atsuya Tanaka <tenmusu007@gmail.com>
2022-06-14 09:55:46 -0700

    changed url

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ export async function gettingData(city = "vancouver"){
     // console.log(city)
     try{
         // const response = await fetch(`https:api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey.weather}&units=metric`)
-        const response = await fetch(`https:api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey.weather}&units=metric`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey.weather}&units=metric`)
         // console.log(response)
         const myJson = await response.json()
         let data = {...myJson};
@@ -19,7 +19,7 @@ export async function gettingDataFor(city = "vancouver"){
     try{
         // const response = await fetch(`https:api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey.weather}&units=metric`)
         // api.openweathermap.org/data/2.5/forecast?q={city name}&appid=6e9e6a1af743311823e535664d4bc3ec&units=metric
-        const response = await fetch(`https:api.openweathermap.org/data/2.5/forecast?q=${city}&appid=6e9e6a1af743311823e535664d4bc3ec&units=metric`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=6e9e6a1af743311823e535664d4bc3ec&units=metric`)
         // console.log(response)
         const myJson = await response.json()
         let data = {...myJson};
